18ea01c678cc0f21b0f3c1cae9dc2d75044b0271
Frankie Gallina-Jones <frankieg@vmware.com>
2022-06-27 17:50:11 -0400

    initial stack definition + tests

---
+++ metadata_test.go
@@ -0,0 +1,264 @@
+package acceptance_test
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	v1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1/layout"
+	. "github.com/onsi/gomega"
+	"github.com/paketo-buildpacks/packit/v2/vacation"
+	"github.com/sclevine/spec"
+
+	. "github.com/paketo-buildpacks/jam/integration/matchers"
+	. "github.com/paketo-buildpacks/packit/v2/matchers"
+)
+
+func testMetadata(t *testing.T, context spec.G, it spec.S) {
+	var (
+		Expect = NewWithT(t).Expect
+
+		tmpDir string
+	)
+
+	it.Before(func() {
+		var err error
+		tmpDir, err = os.MkdirTemp("", "")
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	it.After(func() {
+		Expect(os.RemoveAll(tmpDir)).To(Succeed())
+	})
+
+	it("builds tiny stack", func() {
+		var buildReleaseDate, runReleaseDate time.Time
+
+		by("confirming that the build image is correct", func() {
+			dir := filepath.Join(tmpDir, "build-index")
+			err := os.Mkdir(dir, os.ModePerm)
+			Expect(err).NotTo(HaveOccurred())
+
+			archive, err := os.Open(stack.BuildArchive)
+			Expect(err).NotTo(HaveOccurred())
+			defer archive.Close()
+
+			err = vacation.NewArchive(archive).Decompress(dir)
+			Expect(err).NotTo(HaveOccurred())
+
+			path, err := layout.FromPath(dir)
+			Expect(err).NotTo(HaveOccurred())
+
+			index, err := path.ImageIndex()
+			Expect(err).NotTo(HaveOccurred())
+
+			indexManifest, err := index.IndexManifest()
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(indexManifest.Manifests).To(HaveLen(1))
+			Expect(indexManifest.Manifests[0].Platform).To(Equal(&v1.Platform{
+				OS:           "linux",
+				Architecture: "amd64",
+			}))
+
+			image, err := index.Image(indexManifest.Manifests[0].Digest)
+			Expect(err).NotTo(HaveOccurred())
+
+			file, err := image.ConfigFile()
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(file.Config.Labels).To(SatisfyAll(
+				HaveKeyWithValue("io.buildpacks.stack.id", "io.paketo.stacks.tiny"),
+				HaveKeyWithValue("io.buildpacks.stack.description", "ubuntu:bionic with compilers and shell utilities"),
+				HaveKeyWithValue("io.buildpacks.stack.distro.name", "ubuntu"),
+				HaveKeyWithValue("io.buildpacks.stack.distro.version", "18.04"),
+				HaveKeyWithValue("io.buildpacks.stack.homepage", "https://github.com/paketo-buildpacks/bionic-tiny-stack"),
+				HaveKeyWithValue("io.buildpacks.stack.maintainer", "Paketo Buildpacks"),
+				HaveKeyWithValue("io.buildpacks.stack.metadata", MatchJSON("{}")),
+				HaveKeyWithValue("io.buildpacks.stack.mixins", ContainSubstring(`"build:make"`)),
+				HaveKeyWithValue("io.buildpacks.stack.mixins", ContainSubstring(`"ca-certificates"`)),
+				HaveKeyWithValue("io.paketo.stack.packages", ContainSubstring(`"ca-certificates"`)),
+			))
+
+			Expect(file.Config.Labels).NotTo(HaveKeyWithValue("io.buildpacks.stack.mixins", ContainSubstring("run:")))
+
+			buildReleaseDate, err = time.Parse(time.RFC3339, file.Config.Labels["io.buildpacks.stack.released"])
+			Expect(err).NotTo(HaveOccurred())
+			Expect(buildReleaseDate).NotTo(BeZero())
+
+			Expect(image).To(SatisfyAll(
+				HaveFileWithContent("/etc/group", ContainSubstring("cnb:x:1000:")),
+				HaveFileWithContent("/etc/passwd", ContainSubstring("cnb:x:1000:1000::/home/cnb:/bin/bash")),
+				HaveDirectory("/home/cnb"),
+			))
+
+			Expect(file.Config.User).To(Equal("1000:1000"))
+
+			Expect(file.Config.Env).To(ContainElements(
+				"CNB_USER_ID=1000",
+				"CNB_GROUP_ID=1000",
+				"CNB_STACK_ID=io.paketo.stacks.tiny",
+			))
+
+			Expect(image).To(HaveFileWithContent("/etc/gitconfig", ContainLines(
+				"[safe]",
+				"\tdirectory = /workspace",
+				"\tdirectory = /workspace/source-ws",
+				"\tdirectory = /workspace/source",
+			)))
+
+			Expect(image).To(HaveFileWithContent("/var/lib/dpkg/status", SatisfyAll(
+				ContainSubstring("Package: build-essential"),
+				ContainSubstring("Package: ca-certificates"),
+				ContainSubstring("Package: curl"),
+				ContainSubstring("Package: git"),
+				ContainSubstring("Package: jq"),
+				ContainSubstring("Package: libgmp-dev"),
+				ContainSubstring("Package: libssl1.1"),
+				ContainSubstring("Package: libyaml-0-2"),
+				ContainSubstring("Package: netbase"),
+				ContainSubstring("Package: openssl"),
+				ContainSubstring("Package: tzdata"),
+				ContainSubstring("Package: xz-utils"),
+				ContainSubstring("Package: zlib1g-dev"),
+			)))
+		})
+
+		by("confirming that the run image is correct", func() {
+			dir := filepath.Join(tmpDir, "run-index")
+			err := os.Mkdir(dir, os.ModePerm)
+			Expect(err).NotTo(HaveOccurred())
+
+			archive, err := os.Open(stack.RunArchive)
+			Expect(err).NotTo(HaveOccurred())
+			defer archive.Close()
+
+			err = vacation.NewArchive(archive).Decompress(dir)
+			Expect(err).NotTo(HaveOccurred())
+
+			path, err := layout.FromPath(dir)
+			Expect(err).NotTo(HaveOccurred())
+
+			index, err := path.ImageIndex()
+			Expect(err).NotTo(HaveOccurred())
+
+			indexManifest, err := index.IndexManifest()
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(indexManifest.Manifests).To(HaveLen(1))
+			Expect(indexManifest.Manifests[0].Platform).To(Equal(&v1.Platform{
+				OS:           "linux",
+				Architecture: "amd64",
+			}))
+
+			image, err := index.Image(indexManifest.Manifests[0].Digest)
+			Expect(err).NotTo(HaveOccurred())
+
+			file, err := image.ConfigFile()
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(file.Config.Labels).To(SatisfyAll(
+				HaveKeyWithValue("io.buildpacks.stack.id", "io.paketo.stacks.tiny"),
+				HaveKeyWithValue("io.buildpacks.stack.description", "distroless-like bionic"),
+				HaveKeyWithValue("io.buildpacks.stack.distro.name", "ubuntu"),
+				HaveKeyWithValue("io.buildpacks.stack.distro.version", "18.04"),
+				HaveKeyWithValue("io.buildpacks.stack.homepage", "https://github.com/paketo-buildpacks/bionic-tiny-stack"),
+				HaveKeyWithValue("io.buildpacks.stack.maintainer", "Paketo Buildpacks"),
+				HaveKeyWithValue("io.buildpacks.stack.metadata", MatchJSON("{}")),
+				HaveKeyWithValue("io.buildpacks.stack.mixins", ContainSubstring(`"ca-certificates"`)),
+				HaveKeyWithValue("io.paketo.stack.packages", ContainSubstring(`"ca-certificates"`)),
+			))
+
+			Expect(file.Config.Labels).NotTo(HaveKeyWithValue("io.buildpacks.stack.mixins", ContainSubstring("build:")))
+
+			runReleaseDate, err = time.Parse(time.RFC3339, file.Config.Labels["io.buildpacks.stack.released"])
+			Expect(err).NotTo(HaveOccurred())
+			Expect(runReleaseDate).NotTo(BeZero())
+
+			Expect(file.Config.User).To(Equal("1000:1000"))
+
+			Expect(file.Config.Env).NotTo(ContainElements(
+				"CNB_USER_ID=1000",
+				"CNB_GROUP_ID=1000",
+				"CNB_STACK_ID=io.paketo.stacks.tiny",
+			))
+
+			Expect(image).To(SatisfyAll(
+				HaveFileWithContent("/etc/group", ContainSubstring("cnb:x:1000:")),
+				HaveFileWithContent("/etc/passwd", ContainSubstring("cnb:x:1000:1000::/home/cnb:/sbin/nologin")),
+				HaveDirectory("/home/cnb"),
+			))
+
+			Expect(image).To(SatisfyAll(
+				HaveFile("/usr/share/doc/ca-certificates/copyright"),
+				HaveFile("/etc/ssl/certs/ca-certificates.crt"),
+				HaveDirectory("/root"),
+				HaveDirectory("/tmp"),
+				HaveFile("/etc/services"),
+				HaveFile("/etc/nsswitch.conf"),
+			))
+
+			Expect(image).To(HaveFileWithContent("/var/lib/dpkg/status.d/base-files", SatisfyAll(
+				ContainSubstring("Package: base-files"),
+				MatchRegexp(`Version: [0-9\.]+ubuntu[0-9\.]+`),
+				ContainSubstring("Architecture: amd64"),
+			)))
+
+			Expect(image).To(HaveFileWithContent("/var/lib/dpkg/status.d/ca-certificates", SatisfyAll(
+				ContainSubstring("Package: ca-certificates"),
+				MatchRegexp("Version: [0-9]+"),
+				ContainSubstring("Architecture: all"),
+			)))
+
+			Expect(image).To(HaveFileWithContent("/var/lib/dpkg/status.d/libc6", SatisfyAll(
+				ContainSubstring("Package: libc6"),
+				MatchRegexp("Version: [0-9\\.\\-]+ubuntu[0-9\\.]+"),
+				ContainSubstring("Architecture: amd64"),
+			)))
+
+			Expect(image).To(HaveFileWithContent("/var/lib/dpkg/status.d/libssl1.1", SatisfyAll(
+				ContainSubstring("Package: libssl1.1"),
+				MatchRegexp("Version: [0-9\\.\\-]+ubuntu[0-9\\.]+"),
+				ContainSubstring("Architecture: amd64"),
+			)))
+
+			Expect(image).To(HaveFileWithContent("/var/lib/dpkg/status.d/netbase", SatisfyAll(
+				ContainSubstring("Package: netbase"),
+				MatchRegexp("Version: [0-9\\.]+"),
+				ContainSubstring("Architecture: all"),
+			)))
+
+			Expect(image).To(HaveFileWithContent("/var/lib/dpkg/status.d/openssl", SatisfyAll(
+				ContainSubstring("Package: openssl"),
+				MatchRegexp("Version: [0-9\\.\\-]+ubuntu[0-9\\.]+"),
+				ContainSubstring("Architecture: amd64"),
+			)))
+
+			Expect(image).To(HaveFileWithContent("/var/lib/dpkg/status.d/tzdata", SatisfyAll(
+				ContainSubstring("Package: tzdata"),
+				MatchRegexp("Version: [a-z0-9\\.\\-]+ubuntu[0-9\\.]+"),
+				ContainSubstring("Architecture: all"),
+			)))
+
+			Expect(image).To(HaveFileWithContent("/var/lib/dpkg/status.d/zlib1g", SatisfyAll(
+				ContainSubstring("Package: zlib1g"),
+				MatchRegexp("Version: [a-z0-9\\.\\-\\:]+ubuntu[0-9\\.]+"),
+				ContainSubstring("Architecture: amd64"),
+			)))
+
+			Expect(image).NotTo(HaveFile("/usr/share/ca-certificates"))
+
+			Expect(image).To(HaveFileWithContent("/etc/os-release", SatisfyAll(
+				// TODO: This is different from what we currently ship. Are we ok
+				// with changing?
+				ContainSubstring(`PRETTY_NAME="Paketo Buildpacks Tiny Bionic"`),
+				ContainSubstring(`HOME_URL="https://github.com/paketo-buildpacks/bionic-tiny-stack"`),
+				ContainSubstring(`SUPPORT_URL="https://github.com/paketo-buildpacks/bionic-tiny-stack/blob/main/README.md"`),
+				ContainSubstring(`BUG_REPORT_URL="https://github.com/paketo-buildpacks/bionic-tiny-stack/issues/new"`),
+			)))
+		})
+		Expect(runReleaseDate).To(Equal(buildReleaseDate))
+	})
+}
